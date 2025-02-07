f4e5757616741796c62cd4b68e84e23ad82d1549
Zaurbek Baisangurov <zaur@MacBook-Pro-Zaur.local>
2025-02-07 15:09:50 +0200

    JV-28 fix: приватные параметры вынесены в .env файл. Окружение настроено под его дальнейшее использование.

--- src/main/java/com/projects/timetracker/TimeTrackerApplication.java
+++ src/main/java/com/projects/timetracker/TimeTrackerApplication.java
@@ -1,10 +1,15 @@
 package com.projects.timetracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimeTrackerApplication {
+  static {
+    Dotenv dotenv = Dotenv.load();
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TimeTrackerApplication.class, args);
